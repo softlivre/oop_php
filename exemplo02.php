34576e02f81cbc17c0d56daeb4c2aea5c1d4b8ea
softlivre <softlivre1@gmail.com>
2022-05-15 18:29:26 -0300

    classe carro

---
+++ exemplo02.php
@@ -0,0 +1,65 @@
+<?php
+// show errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class Carro {
+
+    private $modelo;
+    private $motor;
+    private $ano;
+
+    public function getModelo():string {
+        return $this->modelo;
+    }
+
+    public function setModelo($modelo) {
+        $this->modelo = $modelo;
+    }
+
+    public function getMotor(): float {
+        return $this->motor;
+    }
+
+    public function setMotor($motor) {
+        $this->motor = $motor;
+    }
+
+    public function getAno(): int {
+        return $this->ano;
+    }
+
+    public function setAno($ano) {
+        $this->ano = $ano;
+    }
+
+    public function exibir() {
+        return array(
+            "modelo" => $this->getModelo(),
+            "motor" => $this->getMotor(),
+            "ano" => $this->getAno()
+        );
+    }
+
+}
+
+
+$gol = new Carro();
+
+
+$gol->setModelo("Gol GT");
+$gol->setMotor("1.6");
+$gol->setAno("2017");
+
+/*
+echo $gol->getModelo() . "<br>";
+echo $gol->getMotor() . "<br>";
+echo $gol->getAno() . "<br>";
+*/
+
+var_dump($gol->exibir());
+
+
+
+?>
